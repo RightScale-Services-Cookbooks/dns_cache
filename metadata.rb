cf23659a7d970f08cc2fd3936e5d201766a53805
Richard Shade <rshade@rightscale.com>
2014-02-26 11:55:34 -0600

    adding recipe format

--- metadata.rb
+++ metadata.rb
@@ -5,3 +5,6 @@ license          'Apache 2.0'
 description      'Installs/Configures dns_cache'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
+
+
+recipe "dns_cache::default", "installs and sets up dns_cache"
